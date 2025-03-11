a80e6ef818ba4748efe5479fca3874683ecf0e11
Smart, Tim (The Bridge) <tim.smart+azu@astrazeneca.com>
2025-03-11 12:54:08 +0000

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import logo from "./logo.svg";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header>
+          test
+          <img src={logo} />
+        </header>
+        <div>
         {children}
+        </div>
       </body>
     </html>
   );
