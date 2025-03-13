34024fc661ed444bacfc983114f9284b4531826d
Smart, Tim (The Bridge) <tim.smart@timsmarttechnology.co.uk>
2025-03-13 12:58:44 +0000

    fix: header-logo-size updated header logo size and removed unused brand icons from public folder

--- src/_components/header/Header.tsx
+++ src/_components/header/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
   return (
     <header className="relative flex justify-center bg-gradient-to-b from-blue-200 via--blue-100 via-70% via-zinc-50 to-80% to-transparent gap-4 shadow-md">
       <div className="flex flex-col justify-center w-2/3">
-        <div className="w-full self-center mx-0 my-0 sm:w-1/3 sm:mx-4  ">
+        <div className="w-full self-center mx-0 sm:w-1/6 sm:mx-4 my-4  ">
           <TstLogo />
         </div>
 
