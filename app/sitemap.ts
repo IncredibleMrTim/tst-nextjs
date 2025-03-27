d083ef0cf529cc80aa1a981d150717c6a1cb0da9
Tim Smart <tim.smart@timsmarttechnology.co.uk>
2025-03-27 17:33:59 +0000

    chore: robots-sitemap added robots.txt and sitemap.xml (#23)

---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+﻿import type { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://www.timsmarttechnology.co.uk/',
+      lastModified: new Date(),
+      changeFrequency: 'always',
+      priority: 1
+    },
+    {
+      url: 'https://www.timsmarttechnology.co.uk/experience',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8
+    },
+    {
+      url: 'https://www.timsmarttechnology.co.uk/skills',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5
+    }
+  ];
+}
