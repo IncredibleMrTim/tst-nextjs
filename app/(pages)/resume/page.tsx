d3fdc286e0f6d84ad8e42975d24f42cc5893a6ac
Smart, Tim (The Bridge) <tim.smart@timsmarttechnology.co.uk>
2025-04-01 21:38:10 +0100

    fix: resume-rotate-phone added rotation warning for mobile devices

--- app/(pages)/resume/page.tsx
+++ app/(pages)/resume/page.tsx
@@ -1,9 +1,28 @@
+'use client';
+
 import PdfViewer from '@components/pdfViewer/PdfViewer';
+import { useLottie } from 'lottie-react';
+import * as animation from './rotating-phone.json';
 
 const Resume = () => {
+  const { View } = useLottie({
+    animationData: animation,
+    loop: true,
+    autoplay: true
+  });
+
   return (
     <div className="flex justify-center items-center">
-      <PdfViewer />
+      <div className="hidden md:flex">
+        <PdfViewer />
+      </div>
+      <div className="flex flex-col md:hidden">
+        <p className="text-center  mt-4">
+          The PDF viewer is not supported on mobile devices. Please view the
+          resume on a desktop.
+        </p>
+        <div className="flex h-full mt-16">{View}</div>
+      </div>
     </div>
   );
 };
