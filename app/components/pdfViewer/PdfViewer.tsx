282f461f75e686dcd3e34d11b8d786342fdb5112
Tim Smart <tim.smart@timsmarttechnology.co.uk>
2025-04-07 20:00:39 +0100

    fix: added print nd full screen

--- app/components/pdfViewer/PdfViewer.tsx
+++ app/components/pdfViewer/PdfViewer.tsx
@@ -5,23 +5,30 @@ import { useState, useEffect, ReactElement } from 'react';
 
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
-import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/20/solid';
+import {
+  PrinterIcon,
+  ArrowsPointingOutIcon,
+  ArrowLeftIcon,
+  ArrowRightIcon
+} from '@heroicons/react/20/solid';
 
 import {
   Viewer,
   PageChangeEvent,
   DocumentLoadEvent
 } from '@react-pdf-viewer/core';
-import '@react-pdf-viewer/core/lib/styles/index.css';
 import { toolbarPlugin } from '@react-pdf-viewer/toolbar';
 import {
   pageNavigationPlugin,
   RenderCurrentPageLabelProps
 } from '@react-pdf-viewer/page-navigation';
+import {
+  fullScreenPlugin,
+  RenderEnterFullScreenProps
+} from '@react-pdf-viewer/full-screen';
+import { printPlugin, RenderPrintProps } from '@react-pdf-viewer/print';
 import '@react-pdf-viewer/toolbar/lib/styles/index.css';
-import { Toolbar } from 'radix-ui';
-
-// Set the workerSrc to the locally served worker file
+import '@react-pdf-viewer/core/lib/styles/index.css';
 
 const PdfViewer = () => {
   const [pageNumber, setPageNumber] = useState(1);
@@ -53,60 +60,82 @@ const PdfViewer = () => {
   }, [pageNumber, numPages]);
 
   const toolbarPluginInstance = toolbarPlugin();
-  const { Toolbar } = toolbarPluginInstance;
-
   const pageNavigationPluginInstance = pageNavigationPlugin();
   const { jumpToNextPage, jumpToPreviousPage, CurrentPageLabel } =
     pageNavigationPluginInstance;
 
-    const renderNavigation = () => (
-      <CurrentPageLabel>
-        {(props: RenderCurrentPageLabelProps) => (
-          <Flex className="gap-2 pr-2 items-center py-4">
-            <Button
-              variant="ghost"
-              value="Previous Page"
-              title="Previous Page"
-              onClick={() => jumpToPreviousPage()}
-            >
-              <ArrowLeftIcon className="h-5 w-5" />
-            </Button>
-            <div
-              title={`Showing page ${props.currentPage + 1} of ${props.numberOfPages}`}
-            >
-              <div className="flex text-sm min-w-35 justify-center">
-                Showing page {props.currentPage + 1} of {props.numberOfPages}
-              </div>
-            </div>
-            <Button
-              title="Next Page"
-              variant="ghost"
-              value="Next Page"
-              onClick={() => jumpToNextPage()}
-            >
-              <ArrowRightIcon className="h-5 w-5" />
-            </Button>
-          </Flex>
-        )}
-      </CurrentPageLabel>
-    );
+  const fullScreenPluginInstance = fullScreenPlugin();
+  const { EnterFullScreen } = fullScreenPluginInstance;
 
-  const renderToolbar = () => {
-    return (
-      <div className="flex items-center w-full justify-between">
-        <div>{renderNavigation()}</div>
-      </div>
-    );
-  };
+  const printPluginInstance = printPlugin();
+  const { Print } = printPluginInstance;
+
+  const renderToolbar = () => (
+    <CurrentPageLabel>
+      {(props: RenderCurrentPageLabelProps) => (
+        <Flex className="gap-2 pr-2 items-center py-4 !w-full" justify="between">
+          <Flex>
+          <Button
+            variant="ghost"
+            value="Previous Page"
+            title="Previous Page"
+            onClick={() => jumpToPreviousPage()}
+          >
+            <ArrowLeftIcon className="h-5 w-5 text-black" />
+          </Button>
+          <div
+            title={`Showing page ${props.currentPage + 1} of ${props.numberOfPages}`}
+          >
+            <div className="flex text-sm min-w-35 justify-center">
+              Showing page {props.currentPage + 1} of {props.numberOfPages}
+            </div>
+          </div>
+          <Button
+            title="Next Page"
+            variant="ghost"
+            value="Next Page"
+            onClick={() => jumpToNextPage()}
+          >
+            <ArrowRightIcon className="h-5 w-5 text-black" />
+          </Button>
+          </Flex>
+          <Flex className="gap-4">
+          <EnterFullScreen>
+            {(props: RenderEnterFullScreenProps) => (
+              <Button onClick={() => props.onClick()} variant='ghost'>
+                <ArrowsPointingOutIcon className="h-5 w-5 text-black" />
+              </Button>
+            )}
+          </EnterFullScreen>
+          <Print>
+            {(props: RenderPrintProps) => (
+              <Button onClick={() => props.onClick()} variant="ghost">
+                <PrinterIcon className="h-5 w-5 text-black" />
+              </Button>
+            )}
+          </Print>
+          </Flex>
+        </Flex>
+      )}
+    </CurrentPageLabel>
+  );
+
+
 
   return (
-    <div>
-      <div className="mt-8">{renderToolbar()}</div>
+    <div className='flex w-full flex-col'>
 
-      <div className="flex w-[64rem] !h-[50rem]">
+      <div className="mt-8">{renderToolbar()}</div>
+      <div className="flex w-[64rem] flex-col">
         <Viewer
           onPageChange={(e: PageChangeEvent) => setPageNumber(e.currentPage)}
-          plugins={[toolbarPluginInstance, pageNavigationPluginInstance]} // Ensure this matches the declared instance
+          defaultScale={1.6}
+          plugins={[
+            toolbarPluginInstance,
+            pageNavigationPluginInstance,
+            fullScreenPluginInstance,
+            printPluginInstance
+          ]} // Ensure this matches the declared instance
           fileUrl="/TimSmartCV_07-04-25.pdf"
           onDocumentLoad={(e: DocumentLoadEvent) =>
             onDocumentLoadSuccess({ numPages: e.doc.numPages })
@@ -114,7 +143,6 @@ const PdfViewer = () => {
         />
       </div>
     </div>
-
   );
 };
 
