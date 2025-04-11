18f02d489df0a50c205d1bdf036510598ec5689e
Smart, Tim (The Bridge) <tim.smart@timsmarttechnology.co.uk>
2025-04-11 13:38:23 +0100

    added mode loader to nextConfig

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.node$/,
+      use: 'node-loader'
+    });
+
+    return config;
+  },
   images: {
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
